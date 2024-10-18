60d8c20371bb23f972f4661b1cbaf7cc0e406a2d
Cotton Hou <himcotton@gmail.com>
2024-10-18 23:54:59 +0800

    add assets

---
+++ assets.ts
@@ -0,0 +1,41 @@
+export interface Mount {
+
+    readonly href: string;
+    readonly remote: string;
+
+}
+
+
+
+
+
+export const pico_css = {
+
+    version: '2.0.6',
+
+    integrity: 'sha256-3V/VWRr9ge4h3MEXrYXAFNw/HxncLXt9EB6grMKSdMI=',
+
+    Accept: 'text/css',
+
+    get href () {
+        return `/static/css/pico/${ this.version }/pico.min.css`;
+    },
+
+    base: 'https://esm.sh/@picocss/pico',
+
+    get remote () {
+        return `${ this.base }@${ this.version }/css/pico.min.css`;
+    },
+
+} as const;
+
+
+
+
+
+export const collection: readonly Mount[] = [
+
+    pico_css,
+
+];
+
