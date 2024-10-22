43259de68caaa51185a9be9f9f09c612e679dad1
Cotton Hou <himcotton@gmail.com>
2024-10-22 17:13:39 +0800

    add deploy/deno

---
+++ deploy/deno.ts
@@ -0,0 +1,51 @@
+import type { MiddlewareHandler } from 'hono/types';
+
+import { create_app } from '../app.tsx';
+import { gen_deno_kv } from '../adapter/deno-kv.ts';
+import { gen_fnv1a_hash } from '../utils.ts';
+
+
+
+
+
+export function make ({
+
+        auth, kv_path, cache_name, hash_seed, hash_enlarge,
+        signing_nav, signing_site,
+
+}: {
+
+        auth?: MiddlewareHandler,
+        kv_path?: string,
+        cache_name?: string,
+        hash_seed?: string,
+        hash_enlarge?: boolean,
+        signing_nav?: boolean,
+        signing_site?: string,
+
+} = {}): Promise<Deno.ServeDefaultExport> {
+
+    return create_app(gen_deno_kv(kv_path), {
+
+        auth,
+        cache_name,
+        signing_nav,
+        signing_site,
+
+        encoding: gen_fnv1a_hash({
+            key: hash_seed,
+            large: hash_enlarge,
+        }),
+
+    });
+
+}
+
+
+
+
+
+const app: Deno.ServeDefaultExport = await make();
+
+export default app;
+
