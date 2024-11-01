5f3623daa57cd5806cf35f344e1585a83b69e741
Cotton Hou <himcotton@gmail.com>
2024-11-01 12:30:27 +0800

    wip

---
+++ adapter/unstable-cloudflare-kv.ts
@@ -0,0 +1,124 @@
+import { getContext } from 'hono/context-storage';
+
+import * as v from 'valibot';
+
+import type { Adapter, AdapterGen } from './index.ts';
+
+
+
+
+
+export function gen_cloudflare_kv (ns: string): AdapterGen {
+
+    return () => hook(ns);
+
+}
+
+
+
+
+
+function hook (ns: string): Adapter {
+
+    return {
+
+        async get (id) {
+
+            const kv = get_kv(ns);
+            const key = prefix_urls(id);
+
+            const { success, output } = await kv.get(key).then(safe_url);
+
+            if (success === true) {
+                return output;
+            }
+
+        },
+
+        put (id, link, { ttl } = {}) {
+
+            const kv = get_kv(ns);
+            const key = prefix_urls(id);
+
+            const expirationTtl = ttl?.seconds();
+
+            const metadata = {
+                create_date: new Date().toISOString(),
+            };
+
+            return kv.put(key, link, { expirationTtl, metadata }).then(
+                () => true,
+                () => false,
+            );
+
+        },
+
+        async del (id) {
+
+            const kv = get_kv(ns);
+            const key = prefix_urls(id);
+
+            await kv.delete(key);
+
+            return true;
+
+        },
+
+        [Symbol.dispose] () {
+            // noop
+        },
+
+    };
+
+}
+
+
+
+
+
+interface KVNamespace {
+
+    get (key: string): Promise<string>;
+
+    put (key: string, value: string, opts?: {
+
+            expirationTtl?: number,
+            metadata?: object,
+
+    }): Promise<void>;
+
+    delete (key: string): Promise<void>;
+
+}
+
+
+
+
+
+function get_kv (ns: string): KVNamespace {
+
+    const { env } = v.parse(v.object({
+
+        env: v.object({
+
+            [ns]: v.object({
+                get: v.function(),
+                put: v.function(),
+            }),
+
+        }),
+
+    }), getContext());
+
+    return env[ns] as never;
+
+}
+
+
+
+
+
+const safe_url = v.safeParser(v.pipe(v.string(), v.url()));
+
+const prefix_urls = (id: string) => 'v1/urls:'.concat(id);
+
