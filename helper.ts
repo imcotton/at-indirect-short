c807aba4295ac0aa098aa95bfe62dd7224b9fe82
Cotton Hou <himcotton@gmail.com>
2024-11-22 21:49:00 +0800

    hotfix for esbuild plugin from helper

--- helper.ts
+++ helper.ts
@@ -1,7 +1,36 @@
+import type { Plugin } from 'esbuild-deno-loader/esbuild_types';
+
 export * from 'hono/combine';
 
 export { basicAuth } from 'hono/basic-auth';
 export { bearerAuth } from 'hono/bearer-auth';
 
 export { signingAuth } from './utils.ts';
+
+
+
+
+
+export const hotfix: Plugin = {
+
+    name: 'hotfix',
+
+    setup (build) {
+
+        build.onResolve({ filter: /^jsr:/ }, ({ path, kind, resolveDir }) => {
+
+            const fixed = path
+                .replace(/^jsr:\/?@/, 'AT-JSR')
+                .replace('/', '__')
+                .replace(/@[^/]+/, '')
+                .replace('AT-JSR', '@jsr/')
+            ;
+
+            return build.resolve(fixed, { kind, resolveDir });
+
+        });
+
+    },
+
+};
 
