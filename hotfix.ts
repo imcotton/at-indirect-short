e4a2bbd4f62a0fe3b9754e3f42f05d81d8d67023
Cotton Hou <himcotton@gmail.com>
2024-11-23 10:24:06 +0800

    extract hotfix

--- hotfix.ts
+++ hotfix.ts
@@ -4,7 +4,7 @@ import type { Plugin } from 'jsr:@luca/esbuild-deno-loader@0.11.0/esbuild_types'
 
 
 
-export const hotfix: Plugin = {
+export const esbuild_plugin: Plugin = {
 
     name: 'hotfix',
 
